da84ff922a8c83e84009126e0a456a8d7d7a1f3e
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-04-12 17:23:58 +0530

    Product page

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,4 +1,6 @@
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUser , faMobile , faMailBulk} from '@fortawesome/free-solid-svg-icons'
 import Navbar from '../Navbar';
 import Footer from '../Footer'
 const Dashboard = () => {
@@ -6,7 +8,16 @@ const Dashboard = () => {
         <div className='dash-main'>
         <Navbar></Navbar>
         <div className="container">
-            <div className="div-p"></div>
+            <div className='address'></div>
+            <div className='emgcont'></div>
+            <div className="div-p"><FontAwesomeIcon className="user" icon={faUser} color='#ffffff'></FontAwesomeIcon>
+            <p className='name'>The Invisionaries</p>
+            <p className='age-sex'>20 ,<span>Male</span></p>
+            <FontAwesomeIcon className="mobile" icon={faMobile} color='#ababab'></FontAwesomeIcon>
+            <FontAwesomeIcon className="phone" icon={faMailBulk} color='#ababab'></FontAwesomeIcon>
+            <p className='mno'>9560X XXXXX</p>
+            <p className='pno'>example@gmail.com</p>
+            </div>
         </div>
 
         <div className="container2">
--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,4 +1,7 @@
 import './index.scss'
+import p1 from '../../Assets/Images/Product 1.jpg'
+import p2 from '../../Assets/Images/Product 2.jpg'
+import p3 from '../../Assets/Images/Product 3.jpg'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
 import AOS from 'aos';
@@ -12,19 +15,19 @@ const Product = () => {
         <div data-aos="fade-up" data-aos-delay="50" data-aos-duration="1000"
         data-aos-easing="ease-in-out" className='container'>
             <div className='box'>
-                <h1>Product 1</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s.</p>
+                <img src={p1}></img>
+                <h1>Basic Product</h1>
+                <p>Designed for fundamental safety and health monitering. Includes ultrasonic sensor for obstacle detection, BMP sensor for fall detection, and heart rate sensor for pulse monitering.</p>
             </div>
             <div className='box'>
-            <h1>Product 2</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s.</p>
+            <img src={p2}></img>
+            <h1>Advanced Product</h1>
+                <p>Offers comprehensive safety and environmental monitering capabilities. Includes additional features such as camera for object.</p>
             </div>
             <div className='box'>
-            <h1>Product 3</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s.</p>
+            <img src={p3}></img>
+            <h1>Super Product</h1>
+                <p>Tailored specifically for individuals who are blind and deaf. Includes features like camera for real time object character recognition</p>
             </div>
         </div>
         <Footer></Footer>
